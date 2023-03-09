d399a72f03ec84f4fd2a672a1bfba82191cab48f
Junpyo Oh <junpyooh@directional.net>
2023-03-09 12:14:38 +0900

    Initial commit

---
+++ src/main/kotlin/org/bpm/tiljunpyooh/TilJunpyoohApplication.kt
@@ -0,0 +1,11 @@
+package org.bpm.tiljunpyooh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TilJunpyoohApplication
+
+fun main(args: Array<String>) {
+    runApplication<TilJunpyoohApplication>(*args)
+}
---
+++ src/test/kotlin/org/bpm/tiljunpyooh/TilJunpyoohApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bpm.tiljunpyooh
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TilJunpyoohApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
