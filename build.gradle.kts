1230cf44f5a7962cdd85d1cabb90854e804d983f
Junpyo Oh <junpyooh@directional.net>
2023-03-09 12:45:27 +0900

    chore: add dependency aop, batch

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
 
     // kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
